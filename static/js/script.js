1cbd62d66d7ff9cb986ef95a15dc166e8ae7390f
Kiefer <mail@mail.com>
2024-03-14 00:08:44 -0700

    Added some love. Looks good now.

--- static/js/script.js
+++ static/js/script.js
@@ -8,47 +8,63 @@ class UserGenerator {
     console.log('\nUserGenerator instance created.\n\n');
     this.usersObjArray = []; // Array to hold generated user data
     this.employeeID = 1; // Unique ID for each user (employee1, employee2, etc.)
-    this.STAFFCOUNT = 4; // Number of staff members + 1 (for the first 3 users)
+    this.STAFFCOUNT = 3; // Number of staff members
+    this.countStaff = 0; // Counter for staff members
 
+  }
+
+  generateWord() {
+    const words = ["Blorfingle", "Quizzaciously", "Glimboggle", "Frindleplax", "Whompsifer", "Zyxwuvut", "Plumbusque", "Janklefot", "Vexnop", "Grindlewock", "Thwipthwap", "Crumblebuns", "Snerpderp", "Flinglebop", "Quemp", "Lorpzal", "Vlimshard", "Trinklestomp", "Wuzzlefink", "Ploobadoof", "Glempsort", "Froopledox", "Hurpledurp", "Snorklewoggle", "Blampflarf"]
+    return words[Math.floor(Math.random() * words.length)];
+
+  }
+
+  generateEmail() {
+    let emailEndingList = ["gestout", "veleodka", "novidnectar", "gwenrog", "temetonic", "kaermorhe", "dimeritiumht",]
+    let emailHandle = this.generateWord().toLowerCase();
+    let emailEnding = emailEndingList[Math.floor(Math.random() * emailEndingList.length)];
+    return `${emailHandle}@${emailEnding}.com`;
+  }
+
+  generateUsername () {
+    return this.generateWord().toLowerCase();
+  }
+
+  /**
+   * Method to generate a random photo from our /static/images folder
+   * @returns - A random photo
+   */
+  randomPhoto() {
+    const numberOfPhotos = 14; // For example, if you have 10 photos in your folder
+    const photoIndex = Math.floor(Math.random() * numberOfPhotos) + 1; // to get a number between 1 and numberOfPhotos
+    return `/static/img/user${photoIndex}.jpg`; // Adjust the path as per your project structure
   }
 
   /**
    * Method to generate a single user
    * @returns - A user object
    */
-  async generateUser() {
-    try {
-      const response = await fetch('https://randomuser.me/api/?inc=name,email,picture,dob');
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-
-      const data = await response.json();
-      const user = data.results[0]; // Extracting the first user
-
-      return {
-        firstName: user.name.first,
-        lastName: user.name.last,
-        dateOfBirth: user.dob.date,
-        email: user.email,
-        userName: `employee${this.employeeID++}`,
-        // isStaff will be true for the first 3 users, and false for the rest
-        isStaff: this.employeeID <= this.STAFFCOUNT,
-        picture: user.picture.large
-      };
-
-    } catch (error) {
-      console.error('Error fetching user:', error);
-    }
+  generateUser() {
+    return {
+    id: this.employeeID++,
+    firstName: this.generateWord(),
+    lastName: this.generateWord(),
+    userName: this.generateUsername(),
+    email: this.generateEmail(),
+    dateOfBirth: new Date().toISOString().slice(0, 10),
+    picture: this.randomPhoto(),
+    isStaff: this.countStaff++ < this.STAFFCOUNT,
+    department: this.generateWord(),
+    };
   }
 
   /**
    * Method to generate multiple users
    * @param count - The number of users to generate
    */
-  async generateMultipleUsers(count) {
+  generateMultipleUsers(count) {
     for (let i = 0; i < count; i++) {
-      const user = await this.generateUser();
+      const user = this.generateUser();
       if (user) this.usersObjArray.push(user);
     }
   }
@@ -78,23 +94,8 @@ class HumanUser {
    * Confirms that the user has been logged in
    * @returns - True if the user is logged in, false otherwise
    */
-  get isLoggedIn() {
+  get LoggedInStatus() {
     return this.isLogged;
-  }
-
-  /**
-   * Sets the user's login status
-   */
-  set isLoggedIn(status) {
-    this.isLogged = status;
-
-    if (status) {
-      console.log('Setting user as logged in.');
-      this.isLogged = true;
-    } else {
-      console.log('Setting user as logged out.');
-      this.isLogged = false;
-    }
   }
 
   /**
@@ -103,14 +104,6 @@ class HumanUser {
    */
   get isStaffMember() {
     return this.isStaff;
-  }
-
-  /**
-   * Sets a default username and password for the superuser
-   */
-  createAccount() {
-    this.userName = `superuser`;
-    this.password = `password`;
   }
 }
 
@@ -137,7 +130,7 @@ class Website {
 
     navbar.innerHTML = `
    <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">Midterm</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -145,26 +138,8 @@ class Website {
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
       </ul>
-        <button id="navbar-login-button" class="btn btn-outline-success" type="submit">Search</button>
+        <button id="navbar-login-button" class="btn btn-outline-success" type="submit">Login</button>
     </div>
   </div>
     `;
@@ -177,7 +152,11 @@ class Website {
     rowDiv.classList.add('row');
     rowDiv.id = 'card-div';
     containerDiv.appendChild(rowDiv);
+    const cardCarousel = document.createElement('div');
+    cardCarousel.classList.add('card-carousel');
     mainContent.appendChild(containerDiv);
+
+    containerDiv.innerHTML
 
     const footer = document.createElement('footer');
     footer.classList.add('bg-dark', 'text-center', 'text-white', 'py-3');
@@ -187,7 +166,8 @@ class Website {
       </div>
     `;
 
-    mainContent.appendChild(footer);
+    // append footer after main content
+    mainContent.parentNode.insertBefore(footer, mainContent.nextSibling);
 
     const loginModal = document.createElement('div');
     loginModal.classList.add('modal', 'fade');
@@ -222,14 +202,12 @@ class Website {
    * It creates a modal and listens for a click event on the login button.
    */
   initialize() {
-    console.log('\nInitializing login elements...\n\n');
     const modalDialog = document.querySelector('.modal-dialog');
 
     this.controlLoginButton();
 
     // Ensure modal is only created once
     if (!modalDialog) {
-      console.log('Modal does not exist. Creating modal...');
       this.createLoginModal();
     }
   }
@@ -239,7 +217,6 @@ class Website {
    * That is, a form for user to login.
    */
   createLoginModal() {
-    console.log('\nCreating login modal...\n\n');
     const modal = document.getElementById('login-modal');
     const navbar = document.querySelector('.navbar');
 
@@ -261,7 +238,7 @@ class Website {
                                 </svg>
                             </span>
                             <input type="text" class="form-control" id="first-name" placeholder="Enter your first name..."
-                                   required pattern="^[A-Za-z]+$" value="k">
+                                   required pattern="^[A-Za-z]+$" value="JNelson">
                             <div class="valid-feedback text-center">Valid.</div>
                             <div class="invalid-feedback text-center">Please fill out this field.</div>
                         </div>
@@ -275,7 +252,7 @@ class Website {
                                 </svg>
                             </span>
                             <input type="text" class="form-control" id="last-name" placeholder="Enter your last name..."
-                                   required pattern="^[A-Za-z]+$" value="k">
+                                   required pattern="^[A-Za-z]+$" value="JNelson">
                             <div class="valid-feedback text-center">Valid.</div>
                             <div class="invalid-feedback text-center">Please fill out this field.</div>
                         </div>
@@ -283,7 +260,8 @@ class Website {
                     <div class="d-flex flex-column justify-content-center mb-3" id="login-response">
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-primary" id="login-submit-button">Submit</button>
+                        <button type="button" class="btn btn-primary" data-bs-dismiss="modal" id="login-submit-button">Submit</button>
+                        <button type="button" class="btn btn-warning" data-bs-dismiss="modal" id="admin-override">Admin Override</button>
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" id="close-modal-button">Close</button>
                     </div>
                 </div>
@@ -299,41 +277,42 @@ class Website {
    * @param generatedUser - A user object
    */
   generateCard(generatedUser) {
-    //TODO: Refactor this method, why are we using this.htmlContent?
     const cardContainer = document.getElementById('card-div');
 
-    this.htmlContent = `
+    let htmlContent = `
         <div class="col-md-4 my-5 mx-auto generated-cards">
-          <div class="card">
-            <img src="${generatedUser.picture}" class="card-img-top" alt="User Image">
+          <div class="card shadow-md">
+            <img src="${generatedUser.picture}" class="card-img-top img-fluid user-image" alt="User Image #${generatedUser.id}">
             <div class="card-body">
-              <h5 class="card-title">${generatedUser.firstName} ${generatedUser.lastName}</h5>
-              <p class="card-text">Username: ${generatedUser.userName}</p>
-              <p class="card-text">Email: ${generatedUser.email}</p>
-              <p class="card-text">Date of Birth: ${generatedUser.dateOfBirth}</p>
-              <p class="card-text is-staff">Staff Member: ${generatedUser.isStaff ? 'Yes' : 'No'}</p>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item"><h5 class="card-title text-center">${generatedUser.firstName} ${generatedUser.lastName}</h5></li>
+                <li class="list-group-item"><p class="card-text"><span class="fw-bold text-success">Username:</span> ${generatedUser.userName}</p></li>
+                <li class="list-group-item"><p class="card-text"><a href="mailto:${generatedUser.email}">${generatedUser.email}</a></p></li>
+                <li class="list-group-item"><p class="card-text"><span class="fw-bold">Date of Birth:</span> ${generatedUser.dateOfBirth}</p></li>
+                ${this.humanUser.isStaff ? `<li class="list-group-item is-staff"><p class="card-text is-staff"><span class="fw-bold text-success">Staff Member:</span> <span class="text-decoration-underline text-danger fw-bolder">${generatedUser.isStaff ? 'Yes' : 'No'}</span></p></li>` : ''}
+                ${generatedUser.isStaff ? `<li class="list-group-item"><p class="card-text"><span class="fw-bold text-success">Department:</span> ${generatedUser.department}</p></li>` : ''}
+              </ul>
             </div>
-            <div class="staff-button-div">
+            <div class="staff-button-div mx-auto">
             </div>
           </div>
         </div>
     `;
 
-    cardContainer.innerHTML += this.htmlContent;
+    cardContainer.innerHTML += htmlContent;
   }
 
   /**
    * This will be used to add a button to the card if the user is a staff member
    */
   addStaffButton() {
-    console.log('Adding staff button...')
     const staffButtonDiv = document.querySelectorAll('.staff-button-div');
     let increment = 0;
 
     for (const staffButton of staffButtonDiv) {
       const button = document.createElement('button');
-      button.classList.add('btn', 'btn-primary', 'staff-button');
-      button.textContent = 'Staff';
+      button.classList.add('btn', 'btn-warning', 'staff-button', 'mb-3', 'animate__flash', 'animate__delay-2s', 'animate__slow', 'animate__infinite', 'animate__animated');
+      button.textContent = 'Delete User';
       button.id = `staff-button-${increment++}`;
       staffButton.appendChild(button);
     }
@@ -352,21 +331,34 @@ class Website {
   removeSplashPage() {
     // Remove the splash page
     const splashPage = document.getElementById('landing-page');
-    splashPage.remove();
+    splashPage.classList.add('animate__animated', 'animate__fadeOut');
+    splashPage.addEventListener('animationend', () => {
+      splashPage.remove();
+
+      const elementsToAnimate = document.querySelectorAll('.generated-cards');
+      elementsToAnimate.forEach((element) => {
+        element.classList.add('animate__animated', 'animate__fadeIn');
+      });
+      setTimeout(() => {
+        elementsToAnimate.forEach((element) => {
+          element.classList.remove('animate__fadeIn');
+        });
+      }, 2000);
+    });
   }
 
   /**
    * This method will delete users from the website via the Staff button
    */
   deleteUser(event) {
-    console.log('Deleting user...');
-    console.log('Button clicked:', event.id);
+    // TODO: Maybe add a div to confirm the deletion?
+    console.log('Deleting user... ', event.id);
 
     const card = event.closest('.generated-cards');
 
-    // if the card has class "is-staff" and the value is "Yes", console.log("You can't delete a staff member.")
     if (card.querySelector('.is-staff').textContent.includes('Yes')) {
       console.log("You can't delete a staff member.");
+      alert("You can't delete a staff member.");
     } else {
       card.remove();
     }
@@ -378,6 +370,7 @@ class Website {
    */
   controlLoginButton() {
     const navBarButton = document.getElementById('navbar-login-button');
+    const adminOverrideButton = document.getElementById('admin-override');
     const parentElement = navBarButton.parentElement;
 
     if (this.humanUser.isLogged) {
@@ -395,6 +388,8 @@ class Website {
       navBarButton.removeEventListener('click', () => this.logoutUser());
       navBarButton.addEventListener('click', () => this.loginUser());
     }
+
+
   }
 
   /**
@@ -402,7 +397,6 @@ class Website {
    */
   loginUser() {
     //TODO: Clean up the end of this method, the logic is a bit convoluted
-    console.log('\nLogin button clicked.\n\n');
     this.humanUser.isLogged = true;
     this.bootstrapModal = new bootstrap.Modal(document.getElementById('login-modal'));
     this.bootstrapModal.show();
@@ -410,6 +404,7 @@ class Website {
     const form = document.getElementById('login-form');
     const login = document.getElementById('first-name');
     const submitButton = document.getElementById('login-submit-button');
+    const adminOverride = document.getElementById('admin-override');
 
     submitButton.addEventListener('click', (e) => {
       e.preventDefault();
@@ -436,30 +431,23 @@ class Website {
         }
         this.addStaffButton();
       } else {
-        // Only print one user, and all the staff members
         let standardUser = this.userGenerator.allUsers[0];
-        console.log("Stand User: ", standardUser.isStaff);
         if (!standardUser.isStaff) {
-          console.log("Generating standard user...", standardUser);
           this.generateCard(standardUser);
         } else {
-          // grab the first non-staff user
-          let isFound = false;
-          while (!isFound) {
-            for (const generatedUser of this.userGenerator.allUsers) {
-              if (!generatedUser.isStaff) {
-                console.log("User is not Staff, generating card....", generatedUser);
-                this.generateCard(generatedUser);
-                isFound = true;
-                break;
-              }
+          // print the first user that is not staff
+          for (const generatedUser of this.userGenerator.allUsers) {
+            if (!generatedUser.isStaff) {
+              this.generateCard(generatedUser);
+              break;
             }
           }
-        }
-        // this.generateCard(this.userGenerator.allUsers[0]);
-        for (const generatedUser of this.userGenerator.allUsers) {
-          if (generatedUser.isStaff) {
-            this.generateCard(generatedUser);
+
+          // print the staff now
+          for (const generatedUser of this.userGenerator.allUsers) {
+            if (generatedUser.isStaff) {
+              this.generateCard(generatedUser);
+            }
           }
         }
       }
@@ -467,27 +455,36 @@ class Website {
       this.removeSplashPage();
       this.controlLoginButton();
     });
+
+    adminOverride.addEventListener('click', () => {
+      this.humanUser.isLogged = true;
+      this.humanUser.isStaff = true;
+      this.bootstrapModal.hide();
+      this.controlLoginButton();
+      this.removeSplashPage();
+      for (const generatedUser of this.userGenerator.allUsers) {
+        this.generateCard(generatedUser);
+      }
+      this.addStaffButton();
+    });
+
   }
 
   /**
    * Method to log out the user
    */
   logoutUser() {
-    // location.reload();
     console.log('Logging out...');
-    // this.humanUser.isLogged = false;
-    // this.humanUser.isStaff = false;
-    // this.reBuild();
-
-    location.reload();
+    this.humanUser.isLogged = false;
+    this.humanUser.isStaff = false;
+    document.body.innerHTML = document.body.innerHTML.match(/<script[^>]*>[\s\S]*?<\/script>/g).join('');
+    this.startBuilding();
   }
 
   /**
    * Method to start building the website
    */
-  async startBuilding() {
-    console.log('Building website...');
-
+  startBuilding() {
     const mainContent = document.createElement('div');
     mainContent.setAttribute('id', 'main-content');
     document.body.insertBefore(mainContent, document.body.firstChild);
@@ -496,34 +493,19 @@ class Website {
     this.initializeBasicElements();
     this.initialize();
   }
-
-  /**
-   * Method to rebuild the website by removing all elements and starting over
-   */
-  reBuild() {
-    // remove all elements except the script tag
-    const elements = document.body.children;
-    for (let i = 0; i < elements.length; i++) {
-      if (elements[i].tagName !== 'SCRIPT') {
-        elements[i].remove();
-      }
-    }
-    this.startBuilding().then(r => console.log('Rebuilding website...'));
-  }
 }
 
-//TODO: Goldplate the website
-
-document.addEventListener('DOMContentLoaded', async function () {
-  const USER_COUNT = 10;
+const generateAutomation = () => {
+  const USER_COUNT = 15;
 
   const humanUser = new HumanUser();
   const userGenerator = new UserGenerator();
   const website = new Website(humanUser, userGenerator);
 
-  // Async because we are fetching data from an API
-  await website.startBuilding();
-  await userGenerator.generateMultipleUsers(USER_COUNT);
-});
+  website.startBuilding();
+  userGenerator.generateMultipleUsers(USER_COUNT);
+}
+
+generateAutomation();
 
 
